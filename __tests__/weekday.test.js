4db9189b95f91b966152a9ac24d2dd2f08c43e79
Tyler Sinks <tyler.sinks@gmail.com>
2021-03-07 15:14:28 -0500

    good fail day too low spec
    co-authored-by: Marni Sucher <suchermarni@gmail.com>

--- __tests__/weekday.test.js
+++ __tests__/weekday.test.js
@@ -8,4 +8,11 @@ describe('Weekday', () => {
     const result = userDate.message;
     expect(result).toEqual("please enter a valid number day for this month");
   });
+
+  test('should return a string asking for a valid number day of the month, given a day number that is less than 1', () => {
+    const userDate = new Weekday(-5, 0, 2001);
+    userDate.isValidDayNumber();
+    const result = userDate.message;
+    expect(result).toEqual("Please enter a valid day number for this month");
+  });
 });
