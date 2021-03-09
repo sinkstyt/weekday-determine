342efc4ac2da71ddbdea64e22a0e2fd5c23bfeaa
Tyler Sinks <tyler.sinks@gmail.com>
2021-03-09 00:08:14 -0500

    add user interface logic
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import { Weekday, getLastDayOfMonth } from '../src/weekday.js';
+
+$("form#date").submit(function(event) {
+  event.preventDefault();
+  let userYear = parseInt($("#year").val());
+  let userMonth = parseInt($("#month").val());
+  const userDaysMax = getLastDayOfMonth(userYear, userMonth);
+  console.log(userDaysMax);
+  $("input#day-number").attr("max", userDaysMax);
+});
+
+$("form#pick-day-number").submit(function(event) {
+  event.preventDefault();
+  let inputYear = parseInt($("#year").val());
+  let inputMonth = parseInt($("#month").val());
+  let inputDay = parseInt($("#day-number").val());
+  let newWeekday = new Weekday(inputDay, inputMonth, inputYear);
+  newWeekday = newWeekday.whichDay();
+  $("div#result span").html(newWeekday);
+});
